4be60d9b66716222148b99c8862fe54594420df8
David Ritchey <ritchey0713@gmail.com>
2018-03-15 22:09:18 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,3 +5,5 @@ def display_rainbow(color_array)
   puts "R:#{color_array[0]}, O:#{color_array[1]},Y:#{color_array[2]},G:#{color_array[3]},B:#{color_array[4]},I:#{color_array[5]}, V:#{color_array[6]}"
 
 end
+
+display_rainbow()
